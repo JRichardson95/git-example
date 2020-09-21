7aac9b68ef565ddc6fce2ecdfc3261de2f9d9f32
JRichardson95 <jayrichardson@novi-education.nl>
2020-09-21 10:10:08 +0200

    Persoon klasse toegevoegd

--- src/nl/novi/javaprogrammeren/Main.java
+++ src/nl/novi/javaprogrammeren/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Belastingdienst! Jay");
+        Persoon p = new Persoon("Sjaak");
+
+        System.out.println(p.getName());
     }
 }
---
+++ src/nl/novi/javaprogrammeren/Persoon.java
@@ -0,0 +1,13 @@
+package nl.novi.javaprogrammeren;
+
+public class Persoon {
+    private String name;
+
+    public Persoon(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
